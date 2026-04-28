111bd69b793eacd8aec6557ffb119e4f49e9cf85
Klemens Wiesinger <93704497+KlemensJW@users.noreply.github.com>
2026-04-28 13:50:24 +0200

    implemented class_model.py

--- assignments/assignment_1/assignment_1_code/models/class_model.py
+++ assignments/assignment_1/assignment_1_code/models/class_model.py
@@ -16,14 +16,16 @@ class DeepClassifier(nn.Module):
         Saves the model, adds suffix to filename if given
         '''
 
-        ## TODO implement
-        pass
+        filename = "model.pt" if suffix is None else f"model_{suffix}.pt"
+        save_path = save_dir / filename
+        torch.save(self.state_dict(), save_path)
 
     def load(self, path):
         '''
         Loads model from path
         Does not work with transfer model
         '''
+        self.load_state_dict(torch.load(path), weights_only=True)
+        self.eval()
         
-        ## TODO implement
         pass
