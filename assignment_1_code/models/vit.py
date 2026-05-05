28ec0e736ecc6e4007a20f4baa1c8ede90873d97
Klemens Wiesinger <klemens.wiesinger@outlook.at>
2026-05-05 13:34:46 +0200

    removed empty lines

--- assignment_1_code/models/vit.py
+++ assignment_1_code/models/vit.py
@@ -1,6 +1,3 @@
-
-
-
 ## TODO implement your own ViT in this file
 # You can take any existing code from any repository or blog post - it doesn't have to be a huge model
 # specify from where you got the code and integrate it into this code repository so that 
